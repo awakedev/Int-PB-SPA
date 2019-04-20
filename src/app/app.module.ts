7fa2ebd24f0cf88dfba777ee4b48f439140b9687
Nathan C <32592701+awakedev@users.noreply.github.com>
2019-04-20 15:26:25 +0100

    init

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,36 @@
+import { ImageGalleryComponent } from './image-gallery/image-gallery.component';
+import { AboutComponent } from './about/about.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
+import { RouterModule } from '@angular/router';
 
-import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavMenuComponent } from './nav-menu/nav-menu.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavMenuComponent,
+    AboutComponent,
+    ImageGalleryComponent,
+    DashboardComponent
+
   ],
   imports: [
-    BrowserModule,
-    AppRoutingModule
+    BrowserModule.withServerTransition({ appId: 'ng-cli-universal' }),
+    HttpClientModule,
+    FormsModule,
+    RouterModule.forRoot([
+      { path: '', component: DashboardComponent, pathMatch: 'full' },
+      { path: 'about', component: AboutComponent },
+      { path: 'image-gallery', component: ImageGalleryComponent },
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
